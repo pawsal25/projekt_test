3a0f42434d76c7e848cac3b24787901da49cdc20
pawsal25 <pawelsalamon321@gmail.com>
2023-09-11 10:50:57 +0200

    lekcja1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,90 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//
+//        System.out.print("Nasza Lekcja");
+//        System.out.println("Hej!");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        //liczby
+
+        int i = 9;
+        Integer integer = 9;
+
+        Long l = 9L;
+        long l2 = 9L;
+
+        double d = 2.6;
+        Double d2 = 2.3;
+
+        //True False
+
+        boolean b = true;
+        Boolean b2 = true;
+
+        //
+
+        String s = "To Jest Tekst. ";
+        String s2 = " a to jest drugi tekst. ";
+//
+//        System.out.print(s);
+//        System.out.println(s2);
+
+//        int a = i + integer;
+//        System.out.print(i + "+" + integer + "=" + a)
+//
+//        double dwynik = d - d2;
+//        System.out.print(d + "-" + d2 + "=" + dwynik);
+
+//        double potega = Math.pow(d, 2);
+//        potega = Math.round(potega * 100.0) / 100.0;
+//        System.out.print(d + " ^ 2 = " + potega);
+
+//        System.out.println(s.toUpperCase());
+//        System.out.println(s.toLowerCase());
+
+        String non = "";
+        String non1 = "tekst non1";
+        String non2 = null;
+
+//        System.out.println(non.isEmpty());
+//        System.out.println(non1.isEmpty());
+//        System.out.println(non2.isEmpty());
+
+//        if (non != null) {
+//            System.out.println("Nie jest Nullem");
+//        } else {
+//            System.out.println("Jest Nullem");
+//        }
+//
+//        if (non2 != null) {
+//            System.out.println("Nie jest Nullem");
+//        } else {
+//            System.out.println("Jest Nullem");
+//        }
+        if (non != null && !non.isEmpty()) {
+            System.out.println(non.toUpperCase());
+        } else if (non == null){
+            System.out.println("Jest Nullem");
+        } else {
+            System.out.println("Jest Puste");
+        }
+
+
+        if (non1 != null && !non1.isEmpty()) {
+            System.out.println(non1.toUpperCase());
+        } else if (non1 == null){
+            System.out.println("Jest Nullem");
+        } else {
+            System.out.println("Jest Puste");
+        }
+
+
+        if (non2 != null && !non2.isEmpty()) {
+            System.out.println(non2.toUpperCase());
+        } else if (non2 == null){
+            System.out.println("Jest Nullem");
+        } else {
+            System.out.println("Jest Puste");
         }
     }
 }
